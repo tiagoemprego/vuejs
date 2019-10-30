fc64f0cc3ac0325064f0165d9c5e3de544a0b941
tiago marques <tiago@csktech.com.br>
2019-10-30 16:20:52 -0300

    validação de login

---
+++ src/components/popup/PopupError.vue
@@ -0,0 +1,49 @@
+<template>
+    <div v-if="popup.isVisible" id="error">
+        <transition name="fade">
+            <div class="modal fade show" style="display: block; padding-right: 15px;">
+                <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title text-center w-100">{{popup.title}}</h5>
+                            <button @click="closePopup" type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <h5>{{popup.text}}</h5>
+                        </div>
+                        <div class="modal-footer">
+                            <button @click="closePopup" type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </transition>
+        <transition name="fade">
+            <div v-if="popup.isVisible" class="modal-backdrop fade show"></div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "PopupError",
+        props: {
+            popup: {
+                isVisible: false,
+                title: '',
+                text: ''
+            }
+        },
+        methods: {
+            closePopup: function () {
+                this.popup.isVisible = false;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
-        <form @submit.prevent="postFB">
+    <div id="form-register">
+        <div v-if="errors.length">
+            <b>Por favor, corrija o(s) seguinte(s) erro(s):</b>
+            <ul>
+                <li v-for="error in errors" :key="error.id">{{ error }}</li>
+            </ul>
+        </div>
+        <form @submit.prevent="checkForm">
             <div class="form-group">
                 <label for="email">Email</label>
                 <input
@@ -11,7 +17,7 @@
                         placeholder="Email">
             </div>
             <div class="form-group">
-                <label for="password">Nome</label>
+                <label for="password">Senha</label>
                 <input
                         id="password"
                         v-model="password"
@@ -21,30 +27,60 @@
             </div>
             <button type="submit" class="btn btn-success">Submit</button>
         </form>
+        <PopupError :popup="popup"/>
     </div>
 </template>
 
 <script>
+    import PopupError from '@/components/popup/PopupError.vue';
+
     export default {
         name: "Login",
+        components: {
+            PopupError
+        },
         data(){
             return {
                 email: 'tiago@gmail.com',
-                password: '123123'
+                password: '123123',
+                errors: [],
+                popup: {
+                    isVisible: false,
+                    title: '',
+                    text: ''
+                }
             }
         },
         methods: {
             async postFB(){
                 const {email, password} = this;
-
                 try {
                     const  res = await this.$firebase.auth().signInWithEmailAndPassword(email, password);
                     window.uid = res.user.uid;
                     this.$router.push({name: 'home'});
                 }catch (err) {
                     window.console.log(err.code);
-                    window.console.log(err.message)
+                    window.console.log(err.message);
+
+                    this.popup.isVisible = true;
+                    this.popup.title = err.code;
+                    this.popup.text = err.message;
                 }
+            },
+            checkForm: function () {
+                this.errors = [];
+                if (!this.email) {
+                    this.errors.push('O e-mail é obrigatório.');
+                } else if (!this.validEmail(this.email)) {
+                    this.errors.push('Utilize um e-mail válido.');
+                }
+
+                if (!this.errors.length)
+                    this.postFB();
+            },
+            validEmail: function (email) {
+                const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+                return re.test(email);
             }
         },
         mounted() {
